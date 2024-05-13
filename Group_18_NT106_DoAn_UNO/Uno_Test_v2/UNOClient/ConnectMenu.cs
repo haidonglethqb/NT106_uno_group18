fa7dc7000b92e9ddf0d4d87cfffc41dc1ca3da86
HAI <22520378@gm.uit.edu.vn>
2024-05-13 15:25:51 +0700

    final

---
+++ Group_18_NT106_DoAn_UNO/Uno_Test_v2/UNOClient/ConnectMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace CLIENT
+{
+    public partial class ConnectMenu : Form
+    {
+        //no static at first 
+        public static WaitingRoom WaitingRoom;
+        public ConnectMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // TODO: Adding checking for texbox's emptyness, legal values ...
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(textBoxIP.Text), 11000);
+            ProcessSocket.Data = "CONNECT";
+            ProcessSocket.LINK(serverEP);
+            WaitingRoom = new WaitingRoom();
+            ProcessSocket.SENDER(textBoxName.Text);
+
+            User.ID = textBoxName.Text;
+
+            WaitingRoom.FormClosed += new FormClosedEventHandler(lobby_FormClosed);
+            WaitingRoom.ShowStartButton();
+            this.Hide();
+            WaitingRoom.Show();
+        }
+
+        void lobby_FormClosed(object sender, EventArgs e)
+        {
+            ProcessSocket.Data = "DISCONNECT";
+            ProcessSocket.SENDER(User.ID);
+            ProcessSocket.SKCL.Shutdown(System.Net.Sockets.SocketShutdown.Both);
+            ProcessSocket.SKCL.Close();
+            this.Show();
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            // TODO: Adding checking for texbox's emptyness, legal values ...
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(textBoxIP.Text), 11000);
+            ProcessSocket.Data = "CONNECT";
+            ProcessSocket.LINK(serverEP);
+            WaitingRoom = new WaitingRoom();
+            ProcessSocket.SENDER(textBoxName.Text);
+
+            User.ID = textBoxName.Text;
+
+            WaitingRoom.FormClosed += new FormClosedEventHandler(lobby_FormClosed);
+            this.Hide();
+            WaitingRoom.Show();
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {           
+        }
+    }
+}
---
+++ Group_18_NT106_DoAn_UNO/Uno_Test_v2/UNOClient/UNOClient/Form2_Lose.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace UNOClient
+{
+    partial class Form2_Lose
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2_Lose));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(213, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 85);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2_Lose
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(518, 536);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Form2_Lose";
+            this.Text = "Form2_Lose";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
